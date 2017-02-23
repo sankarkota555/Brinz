0b39cff51a6665da8e6fd755bc6934e53d0e076b
Shankar kota <shankar.kota@aa.com>
2017-02-23 17:40:52 +0530

    Created repositories

--- Brinz/Brinz/BrinzData/src/main/java/com/brinz/repository/CustomerRepository.java
+++ Brinz/Brinz/BrinzData/src/main/java/com/brinz/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.brinz.domain.Customer;
 
+
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
   Customer findByCustomerName();
--- Brinz/Brinz/BrinzData/src/main/java/com/brinz/repository/ItemRepository.java
+++ Brinz/Brinz/BrinzData/src/main/java/com/brinz/repository/ItemRepository.java
@@ -1,11 +1,16 @@
 package com.brinz.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.brinz.domain.Item;
 
-public interface ItemRepository extends CrudRepository<Item, Integer>{
-  
-  Item findByItemName();
+public interface ItemRepository extends CrudRepository<Item, Integer> {
+
+  @Query("From Item item where lower(item.itemName) like :itemName%")
+  List<Item> findItemsByName(@Param("itemName") String searchQuery);
 
 }
