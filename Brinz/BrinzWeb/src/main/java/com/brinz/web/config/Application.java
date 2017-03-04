df993d340d2a9d03ea7934b77c8934844c7c4293
Shankar kota <shankar.kota@aa.com>
2017-03-04 16:49:48 +0530

    Project version added

--- Brinz/BrinzWeb/src/main/java/com/brinz/web/config/Application.java
+++ Brinz/BrinzWeb/src/main/java/com/brinz/web/config/Application.java
@@ -1,20 +1,26 @@
 package com.brinz.web.config;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(Application.class);
+    return application.sources(Application.class);
   }
-  
-  public static void main(String args[]) {
+
+  @Bean
+  public String applicationVersion() {
+    System.out.println("app version: " + this.getClass().getPackage().getImplementationVersion());
+    return this.getClass().getPackage().getImplementationVersion();
+  }
+
+  public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 }
