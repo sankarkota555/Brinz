2c9b8916f71551cf08fe621bd1fbd548c177e49f
Shankar kota <shankar.kota@aa.com>
2017-03-08 16:30:12 +0530

    Service added for utility

---
+++ Brinz/BrinzWeb/src/main/webapp/resources/scripts/main/utils/util.service.js
@@ -0,0 +1,78 @@
+"use strict";
+{
+
+    function utilsService(ngDialog) {
+
+        const me = this;
+
+        // show error popup
+        me.processError = function (errorRequest, errorMessage, errorMessageDetails) {
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .clickOutsideToClose(false)
+                    .title(errorMessage + " for " + errorRequest)
+                    .textContent(errorMessageDetails)
+                    .ariaLabel('Alert error message')
+                    .ok('Close')
+                    .openFrom({
+                        top: -50,
+                        width: 30,
+                        height: 80
+                    }).closeTo({
+                        right: 1500
+                    })
+            );
+
+        }; // END - processError()
+
+        // Confirmation popup
+        me.confirmationPopup = function (confirmationMessage, successCallbackFunction, erroCallbackFunction, successParam, errorParam) {
+            const confirmationDialog = $mdDialog.confirm()
+                .title(confirmationMessage)
+                .ariaLabel('confirmation prompt')
+                .ok('YES')
+                .cancel('NO')
+                .openFrom({
+                    top: -50,
+                    width: 30,
+                    height: 80
+                })
+                .closeTo({
+                    right: 1500
+                });
+
+            $mdDialog.show(confirmationDialog).then(function () {
+                successCallbackFunction(successParam);
+            }, function () {
+                erroCallbackFunction(errorParam);
+            });
+
+        }; // END - processError()
+
+        // show alert popup
+        me.alertPopup = function (alertTitle, alertDetails,sucessCallbackFunction) {
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .clickOutsideToClose(false)
+                    .title(alertTitle)
+                    .textContent(alertDetails)
+                    .ariaLabel('Alert infromation message')
+                    .ok('Close')
+                    .openFrom({
+                        top: -50,
+                        width: 30,
+                        height: 80
+                    }).closeTo({
+                        right: 1500
+                    })
+            ).then(function () {
+                sucessCallbackFunction();
+            },null);
+
+        }; // END - processError()
+
+    };
+
+    app.service('utilsService', utilsService);
+
+};
