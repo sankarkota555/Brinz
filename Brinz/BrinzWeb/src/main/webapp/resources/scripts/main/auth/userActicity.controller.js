70f1ba18c873601e2051ca4651e0cba3ad6bb73e
Shankar kota <shankar.kota@aa.com>
2017-03-08 16:29:43 +0530

    User activity added

---
+++ Brinz/BrinzWeb/src/main/webapp/resources/scripts/main/auth/userActicity.controller.js
@@ -0,0 +1,30 @@
+"use strict";
+{
+
+    function userActivityController($scope, userActivityService, utilsService) {
+
+        const me = this;
+        me.logout = function () {
+            userActivityService.logout().then(function (response) {
+                console.log("successfully logged out");
+                console.log(response);
+                userActivityService.logoutSuccess();
+                utilsService.alertPopup('Logout Success!!', null, userActivityService.navigateTologin);
+            },
+                function (response) {
+                    console.log("failed to logout");
+                    // show error opoup
+                    processError(response);
+                });;
+        }
+
+        function processError(response) {
+            utilsService.processError(response.config.url, "Internal Server Error", response.data.errorMessage);
+        }
+
+
+    };
+
+    app.controller('userActivityController', userActivityController);
+
+};
---
+++ Brinz/BrinzWeb/src/main/webapp/resources/scripts/main/auth/userActivity.service.js
@@ -0,0 +1,48 @@
+"use strict";
+{
+
+    function userActivityService($http, $cookies, $window) {
+
+        const me = this;
+
+        /**
+         * Call logout.
+         */
+        me.logout = function () {
+            return $http({
+                url: '/PaymentWeb/logout',
+                method: "POST"
+            });
+        }
+
+        /**
+         * Clear cookes on logout success
+         */
+        me.logoutSuccess = function () {
+            let allCookes = $cookies.getAll();
+            $cookies.remove("XSRF-TOKEN", { path: "/" });
+            $cookies.remove("XSRF-TOKEN", { path: "/PaymentWeb" });
+
+            console.log("all cookes");
+            console.log(allCookes);
+
+            allCookes = $cookies.getAll();
+
+            console.log("all cookes after delete");
+            console.log(allCookes);
+
+        };
+
+        /**
+         *  Redirect to login page.
+         */
+        me.navigateTologin = function () {
+            $window.location.href = "home";
+        }
+
+
+    };
+
+    app.service('userActivityService', userActivityService);
+
+};
