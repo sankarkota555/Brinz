f2f66e38161624b245b76d4d1228cf17aaeafa91
Shankar kota <shankar.kota@aa.com>
2017-04-15 12:18:42 +0530

    Popup code modified as per application

--- Brinz/BrinzWeb/src/main/webapp/resources/scripts/main/utils/util.service.js
+++ Brinz/BrinzWeb/src/main/webapp/resources/scripts/main/utils/util.service.js
@@ -6,57 +6,68 @@
         const me = this;
 
         // show error popup
-        me.processError = function (errorRequest, errorMessage, errorMessageDetails) {
-            ngDialog.open({ template: 'res/scripts/main/menu/menu.template.html', className: 'ngdialog-theme-default', closeByDocument: false});
-
+        me.processError = function (errorMessage, errorMessageDetails) {
+            ngDialog.open({
+                template: '\
+                <div class="Brinz-alert-title">'+ errorMessage + '</div>\
+                <div class="Brinz-alert-message">'+ errorMessageDetails + '</div>\
+                <p class="Brinz-alert-time">'+ new Date() + '</p>\
+                <div class="ngdialog-buttons">\
+                    <button type="button" class="ngdialog-button ngdialog-button-primary" autofocus="true" ng-click="closeThisDialog()">OK</button>\
+                </div>',
+                plain: true
+            });
         }; // END - processError()
 
         // Confirmation popup
         me.confirmationPopup = function (confirmationMessage, successCallbackFunction, erroCallbackFunction, successParam, errorParam) {
-            // preCloseCallback: 
-            const confirmationDialog = $mdDialog.confirm()
-                .title(confirmationMessage)
-                .ariaLabel('confirmation prompt')
-                .ok('YES')
-                .cancel('NO')
-                .openFrom({
-                    top: -50,
-                    width: 30,
-                    height: 80
-                })
-                .closeTo({
-                    right: 1500
-                });
-
-            $mdDialog.show(confirmationDialog).then(function () {
-                successCallbackFunction(successParam);
-            }, function () {
-                erroCallbackFunction(errorParam);
+            const confirmation = ngDialog.open({
+                template: '\
+                <div class="Brinz-confirmation-message">'+ confirmationMessage + '</div>\
+                <div class="ngdialog-buttons">\
+                    <button type="button" class="ngdialog-button ngdialog-button-secondary" ng-click="closeThisDialog(0)">No</button>\
+                    <button type="button" class="ngdialog-button ngdialog-button-primary" autofocus="true" ng-click="closeThisDialog(1)">Yes</button>\
+                </div>',
+                plain: true,
+                closeByEscape: false,
+                closeByDocument: false,
+                showClose: false
             });
 
-        }; // END - processError()
+            confirmation.closePromise.then(function (data) {
+                if (data.value == 1) {
+                    successCallbackFunction(successParam);
+                } else if (erroCallbackFunction) {
+                    erroCallbackFunction(errorParam);
+                }
+                console.log(data.id + ' has been dismissed.');
+            });
+
+
+        }; // END - confirmationPopup()
 
         // show alert popup
-        me.alertPopup = function (alertTitle, alertDetails, sucessCallbackFunction) {
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .clickOutsideToClose(false)
-                    .title(alertTitle)
-                    .textContent(alertDetails)
-                    .ariaLabel('Alert infromation message')
-                    .ok('Close')
-                    .openFrom({
-                        top: -50,
-                        width: 30,
-                        height: 80
-                    }).closeTo({
-                        right: 1500
-                    })
-            ).then(function () {
-                sucessCallbackFunction();
-            }, null);
+        me.alertPopup = function (alertTitle, alertDetails, successCallbackFunction) {
+            const alertPopup = ngDialog.open({
+                template: '\
+                <div class="Brinz-alert-title">'+ alertTitle + '</div>\
+                <div class="Brinz-alert-message">'+ alertDetails + '</div>\
+                <div class="ngdialog-buttons">\
+                    <button type="button" class="ngdialog-button ngdialog-button-primary" autofocus="true" ng-click="closeThisDialog(1)">OK</button>\
+                </div>',
+                plain: true,
+                closeByEscape: false,
+                closeByDocument: false,
+                showClose: false
+            });
 
-        }; // END - processError()
+            alertPopup.closePromise.then(function (data) {
+                if (data.value == 1 && successCallbackFunction) {
+                    successCallbackFunction();
+                }
+            });
+
+        }; // END - alertPopup()
 
     };
 
