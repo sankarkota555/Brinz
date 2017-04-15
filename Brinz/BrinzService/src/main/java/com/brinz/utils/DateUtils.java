fc25922d6c963992ec88ba43493125b031b26f73
Shankar kota <shankar.kota@aa.com>
2017-04-15 17:59:19 +0530

    Date utils added

---
+++ Brinz/BrinzService/src/main/java/com/brinz/utils/DateUtils.java
@@ -0,0 +1,50 @@
+package com.brinz.utils;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class DateUtils {
+
+  /**
+   * Gives system date(current date)
+   * 
+   * @return {@link Date} curretn date
+   */
+  public static Date getCurrentdate() {
+    Instant instant = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant();
+    return Date.from(instant);
+  }
+
+  /**
+   * Adds given number of days to given date.
+   * 
+   * @param date
+   *          {@link Date}
+   * @param numOfDays
+   *          Number of days to be add Example: 1 (adds one day)<br>
+   *          If you want to subtract pass negative value Example: -2 (subtracts 2 days).
+   * @return
+   */
+  public static Date addDaysToDate(Date date, int numOfDays) {
+    if (date != null) {
+      return Date.from(date.toInstant().plus(numOfDays, ChronoUnit.DAYS));
+    }
+    return null;
+  }
+
+  /**
+   * Removes time from given date
+   * 
+   * @param date
+   *          {@link Date} object
+   * @return {@link Date} without time.
+   */
+  public static Date removeTime(Date date) {
+    return Date.from(date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+        .atStartOfDay(ZoneId.systemDefault()).toInstant());
+  }
+
+}
